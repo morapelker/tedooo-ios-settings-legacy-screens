69806e1451e0fb10bd0c328a4ed92582732db2ee
morapelker <morapelker@gmail.com>
2022-07-14 18:21:27 +0300

    "v1.0.1"

---
+++ SettingsLegacyScreens/Classes/SettingsLegacyScreens.swift
@@ -0,0 +1,9 @@
+public protocol SettingsLegacyScreens {
+    
+    func launchBlockedUsers(in navController: UINavigationController)
+    func launchHowToUse(in navController: UINavigationController)
+    func launchInvite(in navController: UINavigationController)
+    func launchContactUs(in navController: UINavigationController)
+    func launchPrivacyPolicy(in navController: UINavigationController)
+    
+}
